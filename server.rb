baace6eac5e77cb6587833affbbc3095af69853b
Ben Balter <ben.balter@github.com>
2015-03-20 15:22:05 -0400

    link to post

--- server.rb
+++ server.rb
@@ -4,13 +4,20 @@ require 'open-uri'
 require 'action_view'
 include ActionView::Helpers::DateHelper
 
-def timestamp
+
+def last_post
   url  = "http://nacin.com/feed/?#{Time.now.to_i}"
   rss  = open(url).read
   feed = RSS::Parser.parse(rss)
+  feed.items.first
+end
+
+def timestamp
   feed.items.first.date
 end
 
+puts
+
 get "/" do
-  erb :index, :locals => { :relative_time => time_ago_in_words(timestamp) }
+  erb :index, :locals => { :relative_time => time_ago_in_words(last_post.date), :link => last_post.link }
 end
